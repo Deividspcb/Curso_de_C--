b3da49167daa1e6206f06013026b03fd21227056
Deividspcb <deividpcb@gmail.com>
2024-06-11 01:16:09 -0300

    while

--- Codigos/10(While).cpp
+++ Codigos/10(While).cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    int cont = 0; // Inicializa a variável cont como 0
+    int num = 0; // Inicializa a variável num como 0
     
     // O loop while continuará executando enquanto a expressão cont++ < 20 for verdadeira.
     // cont++ incrementa o valor de cont após cada iteração do loop.
     // Ou seja, a condição é verificada antes de cont ser incrementado.
 
-    while (cont++ < 20) {
-        cout << "Deivid Lindao - " << cont << "\n"; // Imprime "Deivid Lindao - " seguido pelo valor atual de cont
-        if (cont == 10) { // Se o valor de cont for igual a 10, 
-            break; // O comando break encerra imediatamente o loop while.
-                   // Isso significa que, quando cont chegar a 10, o loop será interrompido e o controle passará para o código após o loop.
+    while (num++ < 20) {
+        cout << "Deivid Lindao - " << num << "\n"; // Imprime "Deivid Lindao - " seguido pelo valor atual de num
+        if (num == 10) { // Se o valor de num for igual a 10, 
+            break;; // O comando break encerra imediatamente o loop while.
+                   // Isso significa que, quando num chegar a 10, o loop será interrompido e o controle passará para o código após o loop.
         }
     }
     
